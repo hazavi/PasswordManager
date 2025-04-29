aa12e914849c8ea4ebe22d90a4b9841cafc21e6c
hazavi <sanchaiavila@gmail.com>
2025-04-29 16:54:11 +0200

    initialize

---
+++ app/src/main/java/com/example/passwordmanager/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.passwordmanager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+//import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        button = findViewById(R.id.btn_time);
+        text = findViewById(R.id.txt_time);
+        String lunchMessage = getString(R.string.lunch_time);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("Setting Text", "Here we get"  );
+                text.setText(lunchMessage);
+            }
+        });
+    }
+}
